016699041fe5b7d315c1c98e0a76c4fe32cd6b37
Żaneta <kul.zaneta@gmail.com>
2020-10-12 12:00:11 +0200

    stage 3

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -4,18 +4,34 @@ import java.util.Scanner;
 
 public class CoffeeMachine {
     public static void main(String[] args) {
-        System.out.println("Write how many cups of coffee you will need:");
         Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Write how many ml of water the coffee machine has:");
+        int amountWater = scanner.nextInt();
+
+        System.out.println("Write how many ml of milk the coffee machine has:");
+        int amountMilk = scanner.nextInt();
+
+        System.out.println("Write how many grams of coffee beans the coffee machine has::");
+        int amountCoffeeBeans = scanner.nextInt();
+
+        System.out.println("Write how many cups of coffee you will need:");
         int cupOfCoffee = scanner.nextInt();
 
-        int water = 200 * cupOfCoffee;
-        int milk = 50 * cupOfCoffee;
-        int coffeeBeans = 15 * cupOfCoffee;
 
-        System.out.println("For " + cupOfCoffee + " cups of coffee you will need:");
-        System.out.println(water + " ml of water");
-        System.out.println(milk + " ml of milk");
-        System.out.println(coffeeBeans + " g of coffee beans");
+        int waterCups = amountWater / 200;
+        int milkCups = amountMilk / 50;
+        int beansCups = amountCoffeeBeans / 15;
 
+        int limit = Math.min(waterCups, Math.min(milkCups, beansCups));
+
+        if (cupOfCoffee > limit) {
+            System.out.println("No, I can make only " + limit + " cup(s) of coffee");
+        } else if (cupOfCoffee == limit) {
+            System.out.println("Yes, I can make that amount of coffee");
+        } else {
+            int extra = limit - cupOfCoffee;
+            System.out.println("Yes, I can make that amount of coffee (and even " + extra + " more than that)");
+        }
     }
 }
